c6fc99c8b19bcd8ce0b4afc06c7e1b72325f5fe5
xauravww <sauravmaheshwari8@gmail.com>
2023-09-23 11:48:51 +0530

    CRUD operations using mongoose

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-import { dbConnect } from "./mongodb.js"
+import mongoose from "mongoose"
+mongoose.connect("mongodb://localhost:27017/e-comm")
+const ProductsSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+  category: String
+})
+const saveInDB = async () => {
+  const ProductsModel = mongoose.model("products", ProductsSchema)
 
-const getData = async () => {
-  let result = await dbConnect()
-  let data = await result.find().toArray()
+  let data = new ProductsModel({
+    name: "poco m2",
+    price: 1000,
+    brand: "xiaomi",
+    category: "mobile"
+  })
+  let result = await data.save()
+
+  console.log(result)
+}
+
+const updateInDB = async () => {
+  const Products = mongoose.model("products", ProductsSchema)
+  let data = await Products.updateOne(
+    { name: "poco m2" },
+    {
+      $set: {
+        price: 14000
+      }
+    }
+  )
+
   console.log(data)
 }
 
-// const getData = () => {
-//   dbConnect().then((resp) => {
-//     resp
-//       .find()
-//       .toArray()
-//       .then((data) => {
-//         console.log(data)
-//       })
-//   })
-// }
+const deleteInDB = async () => {
+  const Products = mongoose.model("products", ProductsSchema)
+  let data = await Products.deleteMany({
+    name: "poco m2"
+  })
 
-getData()
+  console.log(data)
+}
+
+const findInDB = async () => {
+  const Products = mongoose.model("products", ProductsSchema)
+  let data = await Products.find()
+
+  console.log(data)
+}
+
+// saveInDB()
+// updateInDB()
+// deleteInDB()
+findInDB()
--- lib.js
+++
@@ -1,8 +0,0 @@
-const http = require("http")
-
-http
-  .createServer((req, res) => {
-    res.write("<h1>hello aatma<h1>")
-    res.end()
-  })
-  .listen(4500)

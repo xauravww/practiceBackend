08c8dee57963a2b698bfcb419d1ad30dbf63b260
xauravww <sauravmaheshwari8@gmail.com>
2023-11-13 11:13:56 +0530

    URL path & query parameter parsing

--- nodejsServer.js
+++ nodejsServer.js
@@ -1,19 +1,27 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 const server = http.createServer((req, res) => {
   //   console.log("New Request received")
   //   console.log(req.headers)
-  let log = `${Date.now()}: New Request Received on ${req.url} ` + "\n"
-  switch (req.url) {
+
+  if (req.url === "/favicon.ico") return res.end()
+  const log = `${Date.now()}: New Request Received on ${req.url} ` + "\n"
+
+  const myUrl = url.parse(req.url,true)
+  console.log(myUrl)
+
+  switch (myUrl.pathname) {
     case "/":
       fs.appendFile("log.txt", log, (err, data) => {
         res.end("Hello from server again")
       })
       break
-    case "/about":
+      case "/about":
+          const username= myUrl.query.userName
       fs.appendFile("log.txt", log, (err, data) => {
-        res.end("I am Saurav")
+        res.end(`hi ${username}`)
       })
       break
     default:
